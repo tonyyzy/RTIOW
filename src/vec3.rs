c4ed1bc9e81cb403aa27d0f526372896ddf4c4ed
Tony Yang <tony@tony.tc>
2021-03-20 14:10:43 +0000

    Trunc instead of floor; pub methods

--- src/vec3.rs
+++ src/vec3.rs
@@ -22,19 +22,19 @@ impl Vec3 {
         Self { x, y, z }
     }
 
-    fn x(&self) -> f64 {
+    pub fn x(&self) -> f64 {
         self.x
     }
 
-    fn y(&self) -> f64 {
+    pub fn y(&self) -> f64 {
         self.y
     }
 
-    fn z(&self) -> f64 {
+    pub fn z(&self) -> f64 {
         self.z
     }
 
-    fn length(&self) -> f64 {
+    pub fn length(&self) -> f64 {
         self.length_squared().sqrt()
     }
 
@@ -42,11 +42,11 @@ impl Vec3 {
         self.x.powf(2.0) + self.y.powf(2.0) + self.z.powf(2.0)
     }
 
-    fn dot(&self, rhs: &Vec3) -> f64 {
+    pub fn dot(&self, rhs: &Vec3) -> f64 {
         self.x * rhs.x + self.y * rhs.y + self.z * rhs.z
     }
 
-    fn cross(&self, rhs: &Vec3) -> Self {
+    pub fn cross(&self, rhs: &Vec3) -> Self {
         Vec3 {
             x: self.y * rhs.z - self.z * rhs.y,
             y: self.z * rhs.x - self.x * rhs.z,
@@ -54,7 +54,7 @@ impl Vec3 {
         }
     }
 
-    fn unit_vector(&self) -> Self {
+    pub fn unit_vector(&self) -> Self {
         self.clone() / self.length()
     }
 }
@@ -209,9 +209,9 @@ pub fn write_color(f: &mut Stdout, color: &Color) -> std::io::Result<()> {
     write!(
         f,
         "{} {} {}\n",
-        (255.999 * color.x).floor() as u8,
-        (255.999 * color.y).floor() as u8,
-        (255.999 * color.z).floor() as u8
+        (255.999 * color.x).trunc() as u8,
+        (255.999 * color.y).trunc() as u8,
+        (255.999 * color.z).trunc() as u8
     )?;
     Ok(())
 }
